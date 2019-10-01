0b368cc0573aed1ee7817658c116a12b9a25c05f
brendon <bhssilva@inf.ufpel.edu.br>
2019-10-01 11:56:08 -0300

    adicionando server implementado para testar as funções com websocket (feita em nodejs)

---
+++ websocketServerTest/server.js
@@ -0,0 +1,29 @@
+function add(x, y){
+    return x + y;
+}
+
+//over ws
+var WebSocketServer = new require('ws');
+var JsonRPC = require('simple-jsonrpc-js');
+
+var webSocketServer = new WebSocketServer.Server({
+    host: '0.0.0.0',
+    port: 8090
+}).on('connection', function(ws) {
+    var jrpc = new JsonRPC();
+    ws.jrpc = jrpc;
+
+    ws.jrpc.toStream = function(message){
+        ws.send(message);
+    };
+
+    ws.on('message', function(message) {
+        jrpc.messageHandler(message);
+    });
+
+    jrpc.on('add', ['x', 'y'], add);
+
+    jrpc.call('view.setTitle', 
+    ["Enviando do servidor para o browser"]);
+
+});
